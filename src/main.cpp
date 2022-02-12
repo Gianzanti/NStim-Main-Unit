e1cd7605c5d7669f49d22a435b10cf5bf6d9af43
Fabio Gianzanti <fabio.gianzanti@jbtec.com.br>
2022-02-12 17:43:12 -0300

    feat: starting setup process

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <UnicViewAD.h>
+
+// Defines Slave Unit (2nd Arduino) I2C Address
+#define SLAVE_UNIT_ADDRESS 1
+
+// *******************************************************************************************************************************************
+// * Display Init
+// *******************************************************************************************************************************************
+LCM Lcm(Serial);                 // Init Proculus display
+
 
 void setup() {
-  // put your setup code here, to run once:
+    // setup display
+    Lcm.begin();  // Inicia e configura o LCM
+
+    // setup I2C
+    Wire.begin();
+
+    // setup serial monitor
+    Serial.begin(9600);
+    Serial.println("NStim Setup Process Finished");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    delay(50);
+
+
 }
